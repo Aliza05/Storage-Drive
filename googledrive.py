18f18ab9af1440d274ef280212db59737a1c2de7
Aliza05 <aliza.zia524@gmail.com>
2022-11-25 18:56:37 +0500

    Most of the changes are done, some are left but coding is working fine.

--- googledrive.py
+++ googledrive.py
@@ -1,111 +1,72 @@
-#!/usr/bin/python
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-"""Google Drive Quickstart in Python.
-
-This script uploads a single file to Google Drive.
-"""
-
 from __future__ import print_function
 
 import os
 import os.path
 import io
-import six
 import googleapiclient.http
-import httplib2
-import oauth2client.client
 from storagedrive import StorageDrive
-from pathlib import Path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-# If modifying these scopes, delete the file token.json.
-SCOPES1 = ['https://www.googleapis.com/auth/drive.readonly']
+# Scope to see information about files in Google Drive.
+SCOPES_INFORMATION = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+# Scope to see and download files from Google Drive.
+SCOPES_DOWNLOAD_FILES = ['https://www.googleapis.com/auth/drive.readonly']
 
 
 class GoogleDrive(StorageDrive):
+    creds = None
 
-    def upload(self, file_name):
+    def __init__(self):
+        if os.path.exists('token.json'):
+            self.creds = Credentials.from_authorized_user_file('token.json', SCOPES_INFORMATION)
+            # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if False and self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES_INFORMATION)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
 
-        # OAuth 2.0 scope that will be authorized.
-        # Check https://developers.google.com/drive/scopes for all available scopes.
-        OAUTH2_SCOPE = 'https://www.googleapis.com/auth/drive'
-        # Location of the client secrets.
-        CLIENT_SECRETS = 'credentials.json'
+    def upload(self, file_path):
         # Path to the file to upload.
-        FILENAME = file_name
-        # Metadata about the file.
-        MIMETYPE = 'text/plain'
-        TITLE = os.path.basename(file_name)
-        DESCRIPTION = f'A text document about {Path(file_name).stem}.'
-        # Perform OAuth2.0 authorization flow.
-        flow = oauth2client.client.flow_from_clientsecrets(
-            CLIENT_SECRETS, OAUTH2_SCOPE)
-        flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
-        authorize_url = flow.step1_get_authorize_url()
-        print('Go to the following link in your browser: ' + authorize_url)
-        # `six` library supports Python2 and Python3 without redefining builtin input()
-        code = six.moves.input('Enter verification code: ').strip()
-        credentials = flow.step2_exchange(code)
-        # Create an authorized Drive API client.
-        http = httplib2.Http()
-        credentials.authorize(http)
-        drive_service = build('drive', 'v2', http=http)
-        # Insert a file. Files are comprised of contents and metadata.
-        # MediaFileUpload abstracts uploading file contents from a file on disk.
-        media_body = googleapiclient.http.MediaFileUpload(
-            FILENAME,
-            mimetype=MIMETYPE,
-            resumable=True
-        )
-        # The body contains the metadata for the file.
-        body = {
-            'title': TITLE,
-            'description': DESCRIPTION,
-        }
-        # Perform the request and print the result.
+        file_path = file_path
+        self.__init__()
         try:
-            new_file = drive_service.files().insert(
-                body=body, media_body=media_body).execute()
-            file_title = new_file.get('title')
-            file_desc = new_file.get('description')
-            if file_title == TITLE and file_desc == DESCRIPTION:
-                print(f"File is uploaded \nTitle : {file_title}  \nDescription : {file_desc}")
+            # create drive api client
+            service = build('drive', 'v3', credentials=self.creds)
+
+            file_metadata = {'name': os.path.basename(file_path)}
+            media = MediaFileUpload(file_path,
+                                    mimetype='image/jpeg')
+            file = service.files().create(body=file_metadata, media_body=media,
+                                          fields='id').execute()
+            print(F'File ID: {file.get("id")}')
+
         except HttpError as error:
-            # TODO(developer) - Handle errors from drive API.
-            print(f'An error occurred: {error}')
+            print(F'An error occurred: {error}')
+            file = None
 
     def download(self, real_file_id):
-
         creds = None
         if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES1)
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES_DOWNLOAD_FILES)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if False and creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES1)
+                    'credentials.json', SCOPES_DOWNLOAD_FILES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.json', 'w') as token:
@@ -130,33 +91,12 @@ class GoogleDrive(StorageDrive):
 
         return file.getvalue()
 
-    if __name__ == '__main__':
-        download(real_file_id='1FEu2J_quHbg2nyUYUhuxyI9AZyKARTsM')
-
     def search(self):
-        """Search file in drive location
 
-        Load pre-authorized user credentials from the environment.
-        TODO(developer) - See https://developers.google.com/identity
-        for guides on implementing OAuth2 for the application.
-        """
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-            # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if False and creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-
+        self.__init__()
         try:
             # create drive api client
-            service = build('drive', 'v3', credentials=creds)
+            service = build('drive', 'v3', credentials=self.creds)
             files = []
             page_token = None
             while True:
@@ -179,6 +119,3 @@ class GoogleDrive(StorageDrive):
             files = None
 
         return files
-
-    if __name__ == '__main__':
-        search()
--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 from googledrive import GoogleDrive
 
-#file = input("Enter file path: ")
 driver = GoogleDrive()
-#driver.upload(file)
-#driver.search()
-file = input("Enter file id: ")
-driver.download(file)
-
-
-
-
-
-'''
-from __future__ import print_function
-
-from Google import Create_Service
-
-
-CLIENT_SECRET_FILE = 'credentials.json'
-API_NAME = 'drive'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/drive']
-
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-print(dir(service))
-'''
+file = input("Enter file path: ")
+driver.upload(file)
+# driver.search()
+# file = input("Enter file id: ")
+# driver.download(file)
