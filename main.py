5ca350852d44aa6c27d7940ca2ef0e48b21cf32d
Aliza05 <aliza.zia524@gmail.com>
2022-11-30 01:51:36 +0500

    AWS configuration and code is done. Some refactoring is done but some is still required. I will refactor the remaining part soon.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from googledrive import GoogleDrive
+from AWSs3 import AWSCloud
 
-driver = GoogleDrive()
-file = input("Enter file path: ")
-driver.upload(file)
+# driver = GoogleDrive()
+# file = input("Enter file path: ")
+# driver.upload(file)
 # driver.search()
 # file = input("Enter file id: ")
 # driver.download(file)
+
+aws = AWSCloud()
+aws.search()
+
